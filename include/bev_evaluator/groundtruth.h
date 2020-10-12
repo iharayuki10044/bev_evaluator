7098aa93ecd292f7d6516921aa145b62c198346e
iharayuk10044 <ee173138@meiji.ac.jp>
2020-10-12 13:30:07 +0900

    transrate coordinate

--- include/bev_evaluator/groundtruth.h
+++ include/bev_evaluator/groundtruth.h
@@ -10,18 +10,11 @@
 #include <tf/transform_listener.h>
 #include <tf/transform_datatypes.h>
 
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl_ros/point_cloud.h>
-#include <pcl_ros/transforms.h>
-
 #include "Eigen/Core"
 #include "Eigen/Dense"
 #include "Eigen/LU"
 
+#include <pcl/point_cloud.h>
 #include <pcl_ros/transforms.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
@@ -46,18 +39,14 @@ public:
     typedef pcl::PointCloud<PointXYZ>::Ptr CloudXYZPtr;
     typedef pcl::PointCloud<PointN>::Ptr CloudNPtr;
 
-    GroundTruth(void);
-    executor(void);
-    pc_callback(const sensor_msgs::PointCloud2ConstPtr &msg);
-    odom_callback(const nav_msgs::OdometryConstPtr &msg);
-
-    class People
+class People
     {
     public:
         People(void);
 
         double point_x;
         double point_y;
+        double length;
         int point_hit_num;
         double move_vector_x;
         double move_vector_y;
@@ -66,16 +55,21 @@ public:
     };
     typedef std::vector<People> PeopleData;
 
+    GroundTruth(void);
+    executor(void);
+    pc_callback(const sensor_msgs::PointCloud2ConstPtr &msg);
+    odom_callback(const nav_msgs::OdometryConstPtr &msg);
     calculation_peple_point(const cloud_ptr);
     calculation_people_vector(PeopleData&, PeopleData&);
     copy_people_data(PeopleData&, PeopleData&);
-
-    double time_seq;
+    transform_position(PeopleData &pre, double currnet_yow, double pre_yow);
 
 private:
     bool pc_callback_frag = false;
     bool odom_callback_flag = false;
 
+    double current_yow;
+    double pre_yow;
     double RESOLUTION;
     double WIDTH;
     double WIDTH_2;
@@ -83,6 +77,7 @@ private:
     int GRID_WIDTH_2;
     int GRID_NUM;
     int PEOPLE_NUM;
+    int pc_seq;
 
     ros::NodeHandle n;
     ros::NodeHandle nh;
@@ -90,14 +85,16 @@ private:
 	ros::Subscriber odom_subscriber;
 	ros::Publisher bev_grid_publisher;
 
-    PeopleData people_data_new;
-    PeopleData people_data_old;
+    PeopleData current_people_data;
+    PeopleData pre_people_data;
+
+    Eigen::Vector3d current_position;
+    Eigen::Vector3d pre_position;
 
 }
 
-#endif // __GROUNDTRUTH
-
-
+#endif
+// __GROUNDTRUTH
 // while(1){
 //     people_data_new[i].point_x = 5664;
 
--- src/groundtruth.cpp
+++ src/groundtruth.cpp
@@ -9,7 +9,7 @@ GroundTruth::GroundTruth(void)
     local_nh.param("GRID_WIDTH", GRID_WIDTH, {});
     local_nh.param("GRID_WIDTH", GRID_WIDTH_2, {});
     local_nh.param("GRID_NUM", GRID_NUM, {});
-    local_nh.param("PEOPLE_NUM", PEOPLE_NUM, {});
+    local_nh.param("PEOPLE_NUM", PEOPLE_NUM, {30});
 
     pc_subscriber = nh.subscribe("/cloud/dynamic", 10, &GroundTruth::pc_callback, this);
     odom_subscriber = nh.subscribe("/odom", 10, &GroundTruth::odom_callback, this);
@@ -22,12 +22,20 @@ void GroundTruth::executor(void)
 
     ros::Rate r(Hz);
 	while(ros::ok()){
+
         
         if(pc_callback_flag && odm_callback_frag){
-            std::cout << "people calculate" << std::endl;
-            copy_people_data(PeopleData&, PeopleData&);
-            calculation_people_point(cloud_ptr);
-		}
+            std::cout << "people data calculate" << std::endl;
+            copy_people_data(current_people_data, pre_people_data);
+            calcurate_people_point(cloud_ptr);        
+            transform_position(current_people_data;
+    		calcurate_people_vector(current_people_data, pre_people_data);
+        }
+
+        if(IS_READY){
+            std::cout << "generate image" << std::endl;
+            generate_bev_iamge(pre_people_data);
+        }
 
         pc_callback_frag = false;
         odom_callback_flag = false;
@@ -43,6 +51,9 @@ void GroundTruth::formatter(void)
 	/* std::cout << "formatter" << std::endl; */
     dt = 1.0 / Hz;
     grid_size = RANGE / GRID_NUM;
+    cmd_vel_callback_flag = false;
+    pc_callback_flag = false;
+    odom_callback_flag = false;
 }
 
 void GroundTruth::pc_callback(const sensor_msgs::PointCloud2ConstPtr &msg)
@@ -55,20 +66,46 @@ void GroundTruth::pc_callback(const sensor_msgs::PointCloud2ConstPtr &msg)
     pc_callback_flag = true;
 }
 
-void GroundTruth::odom_callback(const nav_msgs::OdometryConstPtr &msg)
+void GroundTruth::cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg)
+//calucurate robot coodinate
 {
-    odom = *msg;
-    odom_callback_flag = true;
+	if(USE_CMD_VEL){
+		geometry_msgs::Twist cmd_vel = *msg;
+		static bool first_flag = false;
+
+		if(!first_flag){
+			initializer();
+			first_flag = true;
+		}
+
+		current_position.x() += cmd_vel.linear.x * dt * cos(current_position.z());
+		current_position.y() += cmd_vel.linear.x * dt * sin(current_position.z());
+		current_yaw += cmd_vel.angular.z * dt;
+
+		while(current_yaw >= M_PI){
+			current_yaw -= 2 * M_PI;
+		}
+		while(current_yaw <= -M_PI){
+			current_yaw += 2 * M_PI;
+		}
+
+		if(step % STEP_BORDER == 0){
+			initializer();
+		}
+
+		cmd_vel_callback_flag = true;
+	}
 }
 
-void GroundTruth::copy_people_data(PeopleData &now, PeopleData &past)
+
+void GroundTruth::copy_people_data(PeopleData &current, PeopleData &pre)
 {
-    past = now;
+    pre = current;
 }
 
-void GroundTruth::calculation_peple_point(const CloudXYZIPtr& cloud_ptr ,People)
+void GroundTruth::calcurate_peple_point(const CloudXYZIPtr& cloud_ptr )
 {
-    std::cout << "--- calculation people point ---" << std::endl;
+    std::cout << "--- calcurate people point ---" << std::endl;
     int cloud_size = cloud_ptr->points.size();
 
     Eigen::Matrix<double, 3, PEOPLE_NUM> people_point = Eigen::MatrixXD::Zero(3, PEOPLE_NUM);
@@ -85,17 +122,61 @@ void GroundTruth::calculation_peple_point(const CloudXYZIPtr& cloud_ptr ,People)
     for(int i=0;i<PEOPLE_NUM;i++){
         
         if(people_point != 0){
-            people_data_now[i].x = people_point(1, id) /people(3, id);
-            people_data_now[i].y = people_point(2, id) /people(3, id);
+            current[i].point_x = people_point(1, id) /people(3, id);
+            current[i].point_y = people_point(2, id) /people(3, id);
+            current[i].length = sqrt(pow(current[i].point_x, 2) +pow(current[i].point_y, 2));   
         }
     }
 }
 
-void GroundTruth::calculation_people_vector(PeopleData &now, PeopleData &past)
+void GroundTruth::calcurate_people_vector(PeopleData &current, PeopleData &pre)
 {
     for(int i=0;i<PEOPLE_NUM;i++){
-        past[i].move_vector_x = now[i].point_x - past[i].point.x;
-        past[i].move_vector_y = now[i].point_y - past[i].point.y;
+        pre[i].move_vector_x = current[i].point_x - pre[i].point.x;
+        pre[i].move_vector_y = current[i].point_y - pre[i].point.y;
     }
 }
 
+void GroundTruth::transform_coordinate(PeopleData &current)
+{
+    double yow = atan2(current_position.x, current_position.y);
+    double current_length = sqrt(pow(current_position.x, 2) +pow(current_position.y, 2));
+    
+    for(int i=0;i<PEOPLE_NUM;i++){
+        current.point_x = current_length *cos(current_yow) + current[i].length *cos(current_yow +yow);
+        current.point_y = current_length *sin(current_yow) + current[i].length *sin(current_yow +yow);
+    }
+}
+
+// void GroundTruth::transform_coordinate(PeopleData &pre, double current_yow, double pre_yow)
+// {
+//     double d_yow = current_yow - pre_yow;
+//     d_yow = atan2(sin(d_yow), cos(d_tow) );
+//     Eigen::Matrix3d r;
+//     r = Eigen::AngleAxisd(-d_yow, Eigen::Vector3d::UnitZ() );
+
+//     Eigen::Matrix3d pre_yow_rotation;
+//     pre_yow_rotation = Eigen::AngleAxisd(-pre_yow, Eigen::Vector3d::UnitZ());
+
+//     Eigen::Vector3d _current_position = pre_yaw_rotation * current_position;
+// 	Eigen::Vector3d _pre_position = pre_yaw_rotation * pre_position;
+// 	Eigen::Translation<double, 3> t(_pre_position - _current_position);
+
+// 	affine_transform = t * r;
+
+// }
+
+void GroundTruth::initializer(void)
+{
+//	std::cout << "initializer" << std::endl;
+	current_position = Eigen::Vector3d::Zero();
+	pre_position = Eigen::Vector3d::Zero();
+	current_yaw = 0.0;
+	pre_yaw = 0.0;
+}
+
+cv::Mat GroundTruth::generate_bev_image(PeopleData &pre)
+{
+
+
+}
